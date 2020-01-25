c4ceb704a242f1602ac02237e601fcafac090b7b
TraceOnBrainOff <sev.webmaster.k@gmail.com>
2020-01-25 21:21:36 +0100

    Separated drawing scripts into 2 files

--- scripts/graph_drawTemp.js
+++ scripts/graph_drawTemp.js
@@ -39,45 +39,6 @@
 //         }
 //     }
 // });
-
-setTimeout(function(){
-    
-    var chartAirQuality= document.getElementById('chart-0').getContext('2d');
-    var chAirQuality = new Chart (chartAirQuality, {
-    type:'bar',
-    data:{
-    labels:['Dobra jakość powietrza','Aktualna jakość powietrza'],
-    datasets:[{
-    label:'Jakość powietrza',
-    data:['50',chartData[1][0]],
-    backgroundColor:['green','red'],
-    borderWidth:1,
-    borderColor:'gray',
-    hoverBorderWidth:3,
-    hoverBorderColor:'black'
-    
-    }]
-    
-    },
-    options: {
-    scales: {
-    yAxes: [{
-    ticks: {
-      beginAtZero: true
-    }
-    }]
-    }
-    }
-    
-    });
-  
-  }, 1000);
-
-
-
-
-
-  
   setTimeout(function(){
     timeWeather=[];
     tempWeather=[];
